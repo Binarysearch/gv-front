c1ea97fef7e2920eb0d5a9c19e6b3fdf84ff8410
Antonio Ruiz <desenlace.net@gmail.com>
2019-03-11 11:56:31 +0100

    Detalles en ventana de planeta

--- src/app/galaxy/planet-window/colony-details/colony-details.component.ts
+++ src/app/galaxy/planet-window/colony-details/colony-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Colony } from 'src/app/game-objects/colony';
 
 @Component({
   selector: 'app-colony-details',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./colony-details.component.css']
 })
 export class ColonyDetailsComponent implements OnInit {
+
+  @Input() colony: Colony;
 
   constructor() { }
 
--- src/app/galaxy/planet-window/planet-window.component.ts
+++ src/app/galaxy/planet-window/planet-window.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Planet } from './../../game-objects/planet';
+import { TextService } from './../../services/text.service';
+import { CoreService } from './../../services/core.service';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-planet-window',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanetWindowComponent implements OnInit {
 
-  constructor() { }
+  @Input() planet: Planet;
+  @Output() closeButton = new EventEmitter();
+  maximized = false;
+  activeTab = 'planet';
+
+  constructor(private core: CoreService, public ts: TextService) { }
 
   ngOnInit() {
+    const statusString = localStorage.getItem('planet-window-status');
+    if (statusString) {
+      const status = JSON.parse(statusString);
+      this.maximized = status.maximized;
+      this.activeTab = status.activeTab;
+    }
   }
 
+  get title(): string {
+    return this.ts.strings.planet + ' ' + this.planet.id;
+  }
+
+  closeButtonClick() {
+    this.closeButton.emit();
+  }
+
+  maximizeButtonClick() {
+    this.maximized = true;
+    this.storeStatus();
+  }
+
+  restoreButtonClick() {
+    this.maximized = false;
+    this.storeStatus();
+  }
+
+  activateTab(tabName: string) {
+    this.activeTab = tabName;
+    this.storeStatus();
+  }
+
+  storeStatus() {
+    const status = {
+      maximized: this.maximized,
+      activeTab: this.activeTab
+    };
+    localStorage.setItem('planet-window-status', JSON.stringify(status));
+  }
 }
